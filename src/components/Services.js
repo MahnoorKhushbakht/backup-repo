288b64152d2f2a773e2d15d2aa08a40c4ead2ef3
mahnoor <mahnoor.bscs3429@iiu.edu.pl>
2024-03-17 06:06:44 +0500

    web backup

---
+++ src/components/Services.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Card1 from './Cards/Card1';
+import Card2 from './Cards/Card2';
+import Card3 from './Cards/Card3';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Typewriter from 'typewriter-effect';
+export default function Variants() {
+  const containerStyles = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center', 
+  };
+
+  return (
+    <Container id="services" sx={containerStyles}>
+      <Typography variant="h4" style={{ marginBottom: '20px', color: '#F6F1EE' ,fontFamily: "'Courier New', Courier, monospace"}}>
+      <Typewriter 
+ onInit={(typewriter) => {
+     typewriter
+         .typeString("Career Snapshot")
+         .start();
+ }}
+
+/>
+      </Typography>
+      <Stack
+        direction={{ xs: 'column', sm: 'row' }}
+        spacing={2}
+        justifyContent="center"
+        alignItems="center"
+        sx={{ margin: '0 2%' }}
+      >
+        <Card1 />
+        <Card2 />
+        <Card3 />
+      </Stack>
+
+    </Container>
+  );
+}
